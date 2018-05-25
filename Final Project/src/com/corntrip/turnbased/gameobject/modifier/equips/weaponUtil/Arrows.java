417724368a75a0eb1c08c5723bf2de1dec17e433
BattleMittens <battlemitten@gmail.com>
2018-05-24 20:13:45 -0400

    Projectile adjustment

--- Final Project/src/com/corntrip/turnbased/gameobject/modifier/equips/weaponUtil/Arrows.java
+++ Final Project/src/com/corntrip/turnbased/gameobject/modifier/equips/weaponUtil/Arrows.java
@@ -1,12 +1,30 @@
 package com.corntrip.turnbased.gameobject.modifier.equips.weaponUtil;
 
+import org.newdawn.slick.GameContainer;
+import org.newdawn.slick.Graphics;
+import org.newdawn.slick.SlickException;
+
+import com.corntrip.turnbased.gameobject.modifier.equips.Weapon;
+import com.corntrip.turnbased.world.World;
+
 public class Arrows extends Projectile
 {
+
+	public Arrows(float startX, float startY, float w, float h, World world, Weapon wep, float rotation) {
+		super(startX, startY, w, h, world, wep, rotation);
+		// TODO Auto-generated constructor stub
+	}
 
 	@Override
 	public float flightSpeed() {
 		// TODO Auto-generated method stub
 		return 0;
+	}
+
+	@Override
+	public void render(GameContainer gc, Graphics gfx, float offsetX, float offsetY) throws SlickException {
+		// TODO Auto-generated method stub
+		
 	}
 
 }
--- Final Project/src/com/corntrip/turnbased/gameobject/modifier/equips/weaponUtil/Projectile.java
+++ Final Project/src/com/corntrip/turnbased/gameobject/modifier/equips/weaponUtil/Projectile.java
@@ -1,32 +1,78 @@
 package com.corntrip.turnbased.gameobject.modifier.equips.weaponUtil;
 
+import java.util.ArrayList;
+
+import org.newdawn.slick.GameContainer;
+import org.newdawn.slick.SlickException;
+
 import com.corntrip.turnbased.gameobject.Entity;
+import com.corntrip.turnbased.gameobject.living.LivingEntity;
 import com.corntrip.turnbased.gameobject.modifier.equips.Weapon;
 import com.corntrip.turnbased.world.World;
 
 public abstract class Projectile extends Entity
 {
-	protected Weapon wep;
+	private Weapon wep;
 	
-	public Projectile(float startX, float startY, float w, float h, World world)
+	private float velX, velY;
+	private float rot;
+	
+	public Projectile(float startX, float startY, float w, float h, World world, Weapon wep, float rotation)
 	{
 		super(startX, startY, w, h, world);
+		this.wep = wep;
+		rot = rotation;
 	}
 	
 	public abstract float flightSpeed();
 	
-	public void endPath(Projectile p)
+	public void endPath()
 	{
-		p = null;
+		getWorld().removeObject(this);
 	}
 	
-	//change this to the weapon items, like the bow's arrows
-	//they should extend the weapon and 'be' it's hitbox
-	public void shoot(Projectile p, float endX, float endY)
+	public void advanceProjectile()
 	{
-		if(p.startX == endX)
-			endPath(p);
+		velX = rot / 90;
+		if(velX > 1)
+		{
+			while(velX > 1)
+			{
+				velX--;
+			}
+			velY = 1-velX;
+		}
+		else if(velX < -1)
+		{
+			while(velX < -1)
+			{
+				velX++;
+			}
+			velY = 1+velX;
+		}
+	}
+	
+	@Override
+	public void update(GameContainer gc, int delta) throws SlickException
+	{
+		advanceProjectile();
 		
-		wep.generateHitbox();
+		ArrayList<Entity> enemiesHit = wep.generateHitbox(super.getX()+velX, super.getY()+velY, super.getWidth(), super.getHeight());
+		
+		for(Entity hitEnemy : enemiesHit)
+		{
+			if(hitEnemy != null)
+			{
+				((LivingEntity) hitEnemy).takeDamage((int)(wep.getDamage()));;
+			}
+		}
+		
+		/*Why doesn't this work?
+		 * for(Entity a : wep.generateHitbox(super.getX()+velX, super.getY()+velY, super.getY(), super.getWidth()));
+		 *	{
+		 *		if( a instanceof LivingEntity)
+		 *			((LivingEntity)a).takeDamage((int)wep.getDamage());
+		 *	}
+		 */
 	}
 }
